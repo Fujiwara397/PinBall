37f785b423092cd0e3fd87d718bde27cbe0c2f5a
藤原靖浩 <fujiwara397@gmail.com>
2019-09-15 23:05:34 +0900

    PinBallプロジェクト　コミット

---
+++ Assets/BallController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+	//ボールが見える可能性のあるz軸の最大値
+	private float visiblePosZ = -6.5f;
+
+	//ゲームオーバを表示するテキスト
+	private GameObject gameoverText;
+
+	//スコアを表示するテキスト
+	private GameObject scoreText;
+	//得点
+	private int score = 0;
+
+
+	// Use this for initialization
+	void Start () {
+
+		//シーン中のGameOverTextオブジェクトを取得
+		this.gameoverText = GameObject.Find("GameOverText");
+
+
+		//シーン中のscoreTextオブジェクトを取得
+		this.scoreText = GameObject.Find("ScoreText");
+
+		// タグによって加点の得点を変える
+		if (tag == "SmallStarTag") {
+			// スコアを加算
+			this.score += 10;
+			} else if (tag == "LargeStarTag") {
+			// スコアを加算
+			this.score += 20;
+			}else if(tag == "SmallCloudTag" || tag == "LargeCloudTag") {
+			// スコアを加算(追加)
+			this.score += 5;
+			
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//ボールが画面外に出た場合
+		if (this.transform.position.z < this.visiblePosZ) {
+
+			//GameoverTextにゲームオーバを表示
+			this.gameoverText.GetComponent<Text> ().text = "Game Over";
+
+		}
+	}
+	void OnCollisionEnter(Collision other){
+		
+		// タグによって加点の得点を変える
+		if (other.gameObject.tag == "SmallStarTag") {
+			// スコアを加算
+			this.score += 10;
+		} else if (other.gameObject.tag == "LargeStarTag") {
+			// スコアを加算
+			this.score += 20;
+		} else if (other.gameObject.tag == "SmallCloudTag" || tag == "LargeCloudTag") {
+			// スコアを加算
+			this.score += 5;
+		}
+
+			//ScoreText獲得した点数を表示
+			this.scoreText.GetComponent<Text> ().text = "Score " + this.score + "pt";
+
+
+	}
+}
